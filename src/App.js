646ecb24de8f418ad5440eb01933a8d9d8de32b2
jeyaram-t <jeyaramtjegan@gmail.com>
2024-04-10 18:27:08 +0530

    Login page UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleClik = () => {
+    navigate("signup");
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className='main'>
+      <div>
+        <header className='header'><h1>
+          Shopping Cart
+        </h1></header>
+        <div className='main-menu'>
+          <div className='menu'></div>
+          <div className='login'>
+            <div className='loginform'>
+              <h2 className='headerTitle'>Login</h2>
+              <div class="row">
+                <label>User name</label>
+                <input description="Username" placeholder="Enter user name" type="text" />
+                <label>Password</label>
+                <input className='inputtwo' description="Password" placeholder="Enter your password" type="password" />
+                <button>Login</button>
+                <button onClick={handleClik}>Sign Up</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
+
   );
 }
 
